ed6182f868cbc7d4cdba8aa4736ebfdb6ebfcfcf
Stv <ai.kaffka@gmail.com>
2014-04-21 22:43:21 +0400

    less2 and wpf-04

---
+++ Lesson 2/Lesson 2/PageFunction1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_2
+{
+    /// <summary>
+    /// Interaction logic for PageFunction1.xaml
+    /// </summary>
+    public partial class PageFunction1 : PageFunction<String>
+    {
+        public PageFunction1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem l = (ListBoxItem)listBox1.SelectedItem;
+            listBox1.Items.Remove(l);
+            listBox2.Items.Add(l);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem l = (ListBoxItem)listBox2.SelectedItem;
+            listBox2.Items.Remove(l);
+            listBox1.Items.Add(l);
+        }
+    }
+}
