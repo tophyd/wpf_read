b92c9ff35ae35907df37c2ab87f362e40c1312ac
Stv <ai.kaffka@gmail.com>
2014-04-23 00:22:45 +0400

    Добавил пример из Петцольда

---
+++ wpf-01p/wpf-01p/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace wpf_01p
+{
+    class main
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var win=new Window {Title = "Say Hello"};
+            win.Show();
+
+            var app=new Application();
+            app.Run();
+        }
+    }
+}
