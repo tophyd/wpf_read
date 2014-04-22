d95cf98cd5907916963a5903b5caf91381f960aa
Stv <ai.kaffka@gmail.com>
2014-04-23 00:35:50 +0400

    Event Handler

--- wpf-01p/wpf-01p/main.cs
+++ wpf-01p/wpf-01p/main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace wpf_01p
 {
@@ -13,9 +14,19 @@ namespace wpf_01p
         {
             var win=new Window {Title = "Say Hello"};
             win.Show();
+            win.MouseDown += WindowOnMouseDown;
 
             var app=new Application();
             app.Run();
+        }
+
+        private static void WindowOnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Window win = sender as Window;
+            string strMessage = string.Format("Window clicked {0} button at point {1}", e.ChangedButton, e.GetPosition(win));
+            MessageBox.Show(strMessage, win.Title);
+
         }
     }
 }
