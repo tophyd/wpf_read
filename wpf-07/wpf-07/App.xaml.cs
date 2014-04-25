ae0a3d1520e874a3af9e807ac9492dcbe9bfbf39
Stv <ai.kaffka@gmail.com>
2014-04-25 21:40:42 +0400

    handeles and events

---
+++ wpf-07/wpf-07/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace wpf_07
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ wpf-07/wpf-07/Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace wpf_07
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+   // public static readonly RoutedEvent sClickEvent;
+
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e) {
+            MessageBox.Show("Event raised by TextBox");
+            e.Handled = (bool)radioButton1.IsChecked;
+
+        }
+
+        private void Grid_TextChanged(object sender, TextChangedEventArgs e) {
+            MessageBox.Show("Event raised by grid");
+            e.Handled = (bool)radioButton2.IsChecked;
+        }
+
+        private void Window_TextChanged(object sender, TextChangedEventArgs e) {
+            MessageBox.Show("Event raised by window");
+            e.Handled = (bool)radioButton3.IsChecked;
+        }
+    }
+}
