630f94b455663f7a7cc000bc241d52c602f54cb4
Stv <ai.kaffka@gmail.com>
2014-04-26 15:10:55 +0400

    Routed command

---
+++ wpf-command/Lesson 2/CustomCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lesson_2 {
+    public class CustomCommands
+    {
+        private static RoutedUICommand launchCommand;
+
+        public static RoutedUICommand Launch
+        {
+            get { return launchCommand; }
+        }
+
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGestures =new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            launchCommand=new RoutedUICommand("Launch", "Launch", typeof(CustomCommands), myInputGestures);
+
+        }
+    }
+}
---
+++ wpf-command/Lesson 2/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            CommandBinding abinding = new CommandBinding();
+            abinding.Command = CustomCommands.Launch;
+            abinding.Executed += Launch_Handler;
+            abinding.CanExecute += LaunchEnabled_Handler;
+            this.CommandBindings.Add(abinding);
+        }
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Launch invoked");
+        }
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (bool)checkBox1.IsChecked;
+        }
+
+        
+
+    }
+}
